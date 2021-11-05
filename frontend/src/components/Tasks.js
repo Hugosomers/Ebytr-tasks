66e7eaac07f0e612d25d0dfb10bcbb8b2ed5bcb4
Hugosomers <hugosomersla@gmail.com>
2021-11-05 17:10:15 -0300

    Adiciona ordenação de listas
    - Ordena por nome
    - Ordena por status

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import { TiSortAlphabeticallyOutline } from 'react-icons/ti';
+import { MdOutlineDoneOutline } from 'react-icons/md';
+import { VscGear } from 'react-icons/vsc';
+
 import axios from 'axios';
 import TaskCard from './TaskCard';
 import './css/tasks.css';
 
 export default function Tasks() {
   const [tasks, setTasks] = useState([]);
+  const [settingsToggle, setSettingsToggle] = useState(false);
   const { updated } = useSelector((state) => state.taskReducer);
+  console.log(tasks);
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -17,11 +23,33 @@ export default function Tasks() {
     fetchTasks();
   }, [updated]);
 
+  const sortTasks = (sortBy) => {
+    const sortedTasks = tasks.sort((a, b) => {
+      return a[sortBy].localeCompare(b[sortBy])
+    });
+    setTasks([...sortedTasks]);
+  }
+
   return (
-    <div className="tasks-container">
-      {!tasks.err && tasks.map((task) => (
-        <TaskCard key={task._id} task={ task } />
-      ))}
+    <div className="tasks-main-div">
+
+      <div className="task-settings-div">
+        <VscGear className="task-settings-icon" onClick={ () => setSettingsToggle(!settingsToggle)}/>
+        {settingsToggle ?
+          <div className="task-settings-div-inner">
+            <TiSortAlphabeticallyOutline className="task-settings-icons-inner" onClick={() => sortTasks('author')}/>
+            <MdOutlineDoneOutline className="task-settings-icons-inner" onClick={() => sortTasks('status')} />
+          </div>
+          :
+          null
+        }
+      </div>
+
+      <div className="tasks-container">
+        {!tasks.err && tasks.map((task) => (
+          <TaskCard key={task._id} task={ task } />
+        ))}
+      </div>
     </div>
   )
 }
